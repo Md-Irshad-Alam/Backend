44ad73e7069e2ab12976b31fba9423646e7024d2
MJACoder <72562331+MJACodeR@users.noreply.github.com>
2023-12-08 19:50:14 +0530

    fix

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(bodyParser.json());
 
 app.use("/api/role-permission", require("./routes/RoleRoutes"))
 app.use("/api/auth", require("./routes/AuthRoute"))
-<<<<<<< HEAD
 app.use("/api/store", require("./routes/StoreRoute"))
 app.use("/api/ForePartCategory", require("./routes/ForePartCategoryRoute"))
 app.use("/api/HeelCategory", require("./routes/HeelCategoryRoute"))
@@ -38,22 +37,6 @@ app.use("/api/UOM",require('./routes/UOMRoute'))
 app.use("/api/AddCategory", require("./routes/AddCategoryRoute"))
 app.use("/api/ProductCategory", require("./routes/ProductCategoryRoute.js"))
 app.use("/api/Productlist",require("./routes/ProductlistRoute.js"))
-=======
-// app.use("/api/store", require("./routes/StoreRoute"))
-// app.use("/api/ForePartCategory", require("./routes/ForePartCategoryRoute"))
-// app.use("/api/HeelCategory", require("./routes/HeelCategoryRoute"))
-// app.use("/api/EmployeeCategory", require("./routes/EmployeeCategoryRoute"))
-// app.use("/api/Ingredient", require("./routes/IngredientRoute"))
-// app.use("/api/Type", require("./routes/TypeRoute"))
-// app.use("/api/designation", require("./routes/DesignationRoute"))
-// app.use("/api/color", require("./routes/ColorMasterRoute"))
-app.use("/api/ArticleGroupMaster", require("./routes/ArticleGroupMasterRoute.js"))
-//  app.use("/api/country", require("./routes/CountryRoute"))
-// app.use("/api/UOM",require('./routes/UOMRoute'))
-// app.use("/api/AddCategory", require("./routes/AddCategoryRoute"))
-// app.use("/api/ProductCategory", require("./routes/ProductCategoryRoute.js"))
-// app.use("/api/Productlist",require("./routes/ProductlistRoute.js"))
->>>>>>> 93c1859df4ada3888ae35e35c3b35adaeafa11d9
 app.use('/api/customer', require('./routes/CustomerRoute'));
 
 
