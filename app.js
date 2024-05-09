669d9dfebc99840b44a32710ef4a95322ebe01b1
Md-Irshad-Alam <almairsh0205@gmail.com>
2024-05-09 15:16:48 +0530

    update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use('/api/state', require('./routes/stateMasterRoute.js'));
 app.use('/api/country', require('./routes/CountryRoute'));
 app.use('/api/UOM', require('./routes/UOMRoute'));
 app.use('/api/AddCategory', require('./routes/AddCategoryRoute'));
-app.use('/api/ProductCategory', require('./routes/ProductCategoryRoute.js'));
+// app.use('/api/ProductCategory', require('./routes/ProductCategoryRoute.js'));
 app.use('/api/Productlist', require('./routes/ProductlistRoute.js'));
 app.use('/api/Product', require('./routes/ProductRoute.js'));
 app.use('/api/customer', require('./routes/CustomerRoute'));
